7b5d2cc9b75cf745dd9794e24cf01348a7555880
GypsyRobot <ramiresero@gmail.com>
2025-05-03 15:54:43 +0200

    healthckeck rpc created

---
+++ backend/main.ts
@@ -0,0 +1,6 @@
+function InitModule(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
+  initializer.registerRpc("healthcheck", rpcHealthcheck);
+  logger.info("Javascript module loaded");
+}
+
+globalThis.InitModule = InitModule;
